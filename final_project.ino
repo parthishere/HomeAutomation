6c7627b0ded5f9d714ed241e5642b519b63fc6bf
parthishere <parthishere1234@gmail.com>
2022-11-07 01:10:50 +0530

    some changes

--- final_project.ino
+++ final_project.ino
@@ -1,6 +1,32 @@
 #include "Fligree.h"
 // #include <ArduinoJson.h>
 #include <SoftwareSerial.h>
+#include <ThingSpeak.h>
+#include <ESP8266HTTPClient.h>
+#include <Servo.h>
+#include <SPI.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 32 // OLED display height, in pixels
+
+// Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
+// The pins for I2C are defined by the Wire-library. 
+// On an arduino UNO:       A4(SDA), A5(SCL)
+// On an arduino MEGA 2560: 20(SDA), 21(SCL)
+// On an arduino LEONARDO:   2(SDA),  3(SCL), ...
+#define OLED_RESET     -1 // Reset pin # (or -1 if sharing Arduino reset pin)
+#define SCREEN_ADDRESS 0x3C ///< See datasheet for Address; 0x3D for 128x64, 0x3C for 128x32
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+
+
+#define LOGO_HEIGHT   16
+#define LOGO_WIDTH    16
+
+
+
 const size_t MAX_CONTENT_SIZE = 512;
 
 #define max_timeout 100
@@ -9,9 +35,14 @@ const size_t MAX_CONTENT_SIZE = 512;
 #define ENDPOINT_GET "api/get/"
 #define ENDPOINT_POST "api/post/"
 
-int sw = 5, led, IC = 0;
+unsigned long counterChannelNumber = 1922816;                // Channel ID
+const char * myCounterReadAPIKey = "2MLZDDQIBA7S319S";      // Read API Key
+const int FieldNumber1 = 1;
+
+int sw = 5, IC = 0;
+const int LED1 = 1;
 //Variables
-int i = 0, pD0 = 0, pD1 = 1, pD2 = 2, pD3 = 3, pD4 = 4, pA0 = 34;
+
 String pin_D0, pin_D1, pin_D2, pin_D3, pin_D4, pin_A0;
 int statusCode;
 unsigned int time_send_scanned_network;
@@ -43,10 +74,15 @@ void manage_bt_data(void);
 Fligree esp;
 SoftwareSerial btSerial(D4, D3);  // RX,
 SoftwareSerial ESPserial(2, 3);
+WiFiClient client;
+Servo myservo;  // create servo object to control a servo
+
 
 void setup() {
 
-
+  myservo.attach(16);
+  pinMode(sw, INPUT);
+  
   btSerial.begin(9600);
   Serial.begin(9600);
   delay(100);
@@ -68,8 +104,27 @@ void setup() {
   delay(100);
 
 
+  if(!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS)) {
+    Serial.println(F("SSD1306 allocation failed"));
+    for(;;); // Don't proceed, loop forever
+  }
 
-  Serial.println("Startup");
+  
+  display.display();
+  delay(1000); 
+
+  display.clearDisplay();
+
+  display.drawPixel(10, 10, SSD1306_WHITE);
+
+  
+
+  display.setTextSize(1);             // Normal 1:1 pixel scale
+  display.setTextColor(SSD1306_WHITE);        // Draw white text
+  display.setCursor(0,0);             // Start at top-left corner
+  display.println(F("Startup"));
+  display.display();
+
 
   Serial.println("Enter 1 for cleaing eeprom");
   while (millis() < max_timeout * 100) {
@@ -94,7 +149,7 @@ void setup() {
     Serial.println("EEPROM isn't cleared !");
   }
 
-  //---------------------------------------- Read EEPROM for SSID and pass
+  // Read EEPROM for SSID and pass
   Serial.println("Reading EEPROM ssid");
 
   String esid;
@@ -142,11 +197,14 @@ void setup() {
   Serial.println("");
 
 
-  pinMode(sw, INPUT);
+  
+  
+  
   WiFi.begin(esid.c_str(), epass.c_str());
   if (checkWifi_connection(uqid, username, password)) {
     Serial.println("Succesfully Connected!!!");
     esp.begin(url, username, password, uqid);
+    ThingSpeak.begin(client);
     return;
   } else {
     Serial.println("Turning the Bluetooth On");
@@ -159,7 +217,6 @@ void setup() {
     else {
       Serial.println("Do nothing");
     }
-
   }
 
   Serial.println();
@@ -169,41 +226,25 @@ void setup() {
     delay(1);
     send_data_to_bt_and_setup_sta();
   }
+
 }
+
+
 
 void loop() {
-  // if (btSerial.available > 0){
-  //   Serial.println(btSerial.read());
-  //   Serial.println("reading form bluetooth");
-  // }
 
-
-
-  if (false) { /*((WiFi.status() == WL_CONNECTED))*/
-
-    pin_D0 = String(digitalRead(D0));
-    pin_D1 = String(digitalRead(D1));
-    pin_D2 = String(digitalRead(D2));
-    pin_D3 = String(digitalRead(D3));
-    pin_D4 = String(digitalRead(D4));
-
-    pin_A0 = String(analogRead(A0));
-
-    while (Serial.available() == 0);
-
-    int a = Serial.parseInt();
-
-    if (a == 1) {
-      payload, code = esp.GETData(ENDPOINT_GET);
-      Serial.println(payload);
-
-
-    } else if (a == 2) {
-      payload, code = esp.POSTData(ENDPOINT_POST, pin_D0, pin_D1, pin_D2, pin_D3, pin_D4, pin_A0);
-    }
-
+  int A = ThingSpeak.readLongField(counterChannelNumber, FieldNumber1, myCounterReadAPIKey);
+  if (A == 1){
+    myservo.write(90);
   }
+  else{
+    myservo.write(0);
+  }
+  Serial.println(A);
+
 }
+
+
 
 bool checkWifi_connection(String username, String password, String uqid) {
   int c = 0;
@@ -223,6 +264,8 @@ bool checkWifi_connection(String username, String password, String uqid) {
   Serial.println("Connection timed out, password or username are incorrect");
   return false;
 }
+
+
 
 void send_data_to_bt_and_setup_sta(void) {
 
@@ -262,14 +305,14 @@ void send_data_to_bt_and_setup_sta(void) {
 
           Serial.println("writing eeprom ssid:");
           for (int i = 0; i < new_ssid_from_bt.length(); ++i) {
-          EEPROM.write(i, new_ssid_from_bt[i]);
+            EEPROM.write(i, new_ssid_from_bt[i]);
             Serial.print("Wrote: ");
             Serial.println(new_ssid_from_bt[i]);
           }
-          
+
           Serial.println("writing eeprom pass:");
           for (int i = 0; i < new_pass_from_bt.length(); ++i) {
-          EEPROM.write(32 + i, new_pass_from_bt[i]);
+            EEPROM.write(32 + i, new_pass_from_bt[i]);
             Serial.print("Wrote: ");
             Serial.println(new_pass_from_bt[i]);
           }
@@ -307,161 +350,3 @@ void send_data_to_bt_and_setup_sta(void) {
   }
 
 }
-
-
-void manage_bt_data(void) {
-  time_send_scanned_network += 1;
-
-  while (btSerial.available() > 0) {
-    Serial.println("reading recieved");
-    char data = btSerial.read();
-    if ((data == '\r') || (data == '\n'))  { // EOL detected
-      buff[buffCount++] = 0; // C strings end with a terminating zero.
-      Serial.println();
-      Serial.println(buff);
-      buffCount = 0; // start the next line
-      memset(buff, 0, sizeof(buff));
-      // buf has the data
-    }
-    else {
-      buff[buffCount++] = data; // packing a byte into the MUMBO JUMBO buffer
-      if (buffCount >= buffCharLimit) { // prevent buffer overflow
-        buff[buffCount++] = 0; // C strings end with a terminating zero.
-        Serial.println();
-        Serial.println(buff);
-        buffCount = 0; // start the next line
-        memset(buff, 0, sizeof(buff));
-      }
-    }
-  }
-
-  if (time_send_scanned_network > 1000) {
-    Serial.println("again send scanned networks");
-    btSerial.write(cString);
-    time_send_scanned_network = 0;
-  }
-
-  /*
-    Serial.println(qsid);
-    Serial.println("");
-    Serial.println(qpass);
-    Serial.println("");
-    Serial.println(username);
-    Serial.println("");
-    Serial.println(password);
-    Serial.println("");
-    Serial.println(uqid);
-    Serial.println("");
-
-    Serial.println("writing eeprom ssid:");
-    for (int i = 0; i < qsid.length(); ++i) {
-    EEPROM.write(i, qsid[i]);
-    Serial.print("Wrote: ");
-    Serial.println(qsid[i]);
-    }
-    Serial.println("writing eeprom pass:");
-    for (int i = 0; i < qpass.length(); ++i) {
-    EEPROM.write(32 + i, qpass[i]);
-    Serial.print("Wrote: ");
-    Serial.println(qpass[i]);
-    }
-
-    Serial.println("writing eeprom username:");
-    for (int i = 0; i < username.length(); ++i) {
-    EEPROM.write(96 + i, username[i]);
-    Serial.print("Wrote: ");
-    Serial.println(username[i]);
-    }
-
-    Serial.println("writing eeprom password:");
-    for (int i = 0; i < password.length(); ++i) {
-    EEPROM.write(128 + i, password[i]);
-    Serial.print("Wrote: ");
-    Serial.println(password[i]);
-    }
-
-    Serial.println("writing eeprom uqid:");
-    for (int i = 0; i < uqid.length(); ++i) {
-    EEPROM.write(196 + i, uqid[i]);
-    Serial.print("Wrote: ");
-    Serial.println(uqid[i]);
-    }
-
-
-    EEPROM.commit();
-    delay(100);
-    ESP.reset();
-  */
-}
-
-
-
-
-
-
-
-
-// void func(String payload){
-//     const size_t capacity = JSON_ARRAY_SIZE(5) + 10*JSON_OBJECT_SIZE(3) + 490;
-//     DynamicJsonBuffer jsonBuffer(capacity);
-
-
-//     JsonObject& root = jsonBuffer.parseObject(payload);
-
-//     Serial.println("Root");
-//     Serial.println(root.size());
-
-//     for (JsonPair& kv : root) {
-//     Serial.println(kv.key);
-//     Serial.println(kv.value.as<char*>());
-//     }
-
-//    JsonObject& root_0 = root[0];
-//    JsonObject& root_0_esp = root_0["esp"];
-//    int root_0_esp_id = root_0_esp["id"]; // 1
-//    const char* root_0_esp_unique_id = root_0_esp["unique_id"]; // "hsgusbbwi162bs"
-//    const char* root_0_esp_timestamp = root_0_esp["timestamp"]; // "2022-07-08T11:00:42.773325Z"
-//
-//    const char* root_0_name = root_0["name"]; // "A0"
-//    int root_0_value = root_0["value"]; // 0
-//
-//    JsonObject& root_1 = root[1];
-//
-//    JsonObject& root_1_esp = root_1["esp"];
-//    int root_1_esp_id = root_1_esp["id"]; // 1
-//    const char* root_1_esp_unique_id = root_1_esp["unique_id"]; // "hsgusbbwi162bs"
-//    const char* root_1_esp_timestamp = root_1_esp["timestamp"]; // "2022-07-08T11:00:42.773325Z"
-//
-//    const char* root_1_name = root_1["name"]; // "D3"
-//    int root_1_value = root_1["value"]; // 0
-//
-//    JsonObject& root_2 = root[2];
-//
-//    JsonObject& root_2_esp = root_2["esp"];
-//    int root_2_esp_id = root_2_esp["id"]; // 1
-//    const char* root_2_esp_unique_id = root_2_esp["unique_id"]; // "hsgusbbwi162bs"
-//    const char* root_2_esp_timestamp = root_2_esp["timestamp"]; // "2022-07-08T11:00:42.773325Z"
-//
-//    const char* root_2_name = root_2["name"]; // "D4"
-//    int root_2_value = root_2["value"]; // 0
-//
-//    JsonObject& root_3 = root[3];
-//
-//    JsonObject& root_3_esp = root_3["esp"];
-//    int root_3_esp_id = root_3_esp["id"]; // 1
-//    const char* root_3_esp_unique_id = root_3_esp["unique_id"]; // "hsgusbbwi162bs"
-//    const char* root_3_esp_timestamp = root_3_esp["timestamp"]; // "2022-07-08T11:00:42.773325Z"
-//
-//    const char* root_3_name = root_3["name"]; // "D2"
-//    int root_3_value = root_3["value"]; // 0
-//
-//    JsonObject& root_4 = root[4];
-//
-//    JsonObject& root_4_esp = root_4["esp"];
-//    int root_4_esp_id = root_4_esp["id"]; // 1
-//    const char* root_4_esp_unique_id = root_4_esp["unique_id"]; // "hsgusbbwi162bs"
-//    const char* root_4_esp_timestamp = root_4_esp["timestamp"]; // "2022-07-08T11:00:42.773325Z"
-//
-//    const char* root_4_name = root_4["name"]; // "D0"
-//    int root_4_value = root_4["value"]; // 0
-// }
